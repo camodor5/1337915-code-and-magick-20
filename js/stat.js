4a51c669ae964845e8031149d8772fac1434156d
camodor5 <suffocationanymore@gmail.com>
2020-06-19 15:10:44 +0300

    Исправил часть замечаний

--- js/stat.js
+++ js/stat.js
@@ -1,24 +1,32 @@
 'use strict';
-
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var BAR_WIDTH = 40;
-var BAR_PADDING = 50;
-var BAR_MAX_HEIGHT = 150;
-var GAP = 10;
-var CLOUD_PADDING = 20;
-
-var GISTO_X = CLOUD_X + CLOUD_PADDING * 2;
-var GISTO_Y = CLOUD_Y + 80;
-
-
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var CLOUD = {
+  width: 420,
+  height: 270,
+  x: 100,
+  y: 10,
+  padding: 20,
 };
 
+var BAR = {
+  width: 40,
+  maxHeight: 150,
+  padding: 50,
+};
+
+var GAP = 10;
+var GISTO_X = CLOUD.x + CLOUD.padding * 2;
+var GISTO_Y = CLOUD.y + 80;
+
+
+var renderRect = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD.width, CLOUD.height);
+};
+
+var renderCloud = function (ctx, x, y, color) {
+  renderRect(ctx, x + GAP, y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderRect(ctx, x, y, color);
+};
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -28,9 +36,9 @@ var getMaxElement = function (arr) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
 };
+
 
 var getRandomNumber = function (max, min) {
   if (min === undefined) {
@@ -40,6 +48,7 @@ var getRandomNumber = function (max, min) {
   return randomNumber;
 };
 
+
 var getBarColor = function (name) {
   var saturation = getRandomNumber(100);
   if (name === 'Вы') {
@@ -47,39 +56,76 @@ var getBarColor = function (name) {
 
   } else {
     var barColor = 'hsl(240, ' + saturation + '% , 50%)';
-    console.log(barColor);
   }
   return barColor;
 };
 
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+var renderIntro = function (ctx, text, x, y, font, fillStyle) {
+  ctx.fillStyle = fillStyle;
+  ctx.font = font;
+  ctx.fillText(text, x, y);
+};
+/*
+var renderResults = function (ctx, players, times) {
+  var roundedTime = Math.round(times[j]);
+  for (var j = 0; j < players.length; j++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(roundedTime, GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + (BAR.maxHeight - barHeight) - CLOUD.padding);
+    ctx.fillText(players[j], GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + BAR.maxHeight + CLOUD.padding);
+  }
+};
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_PADDING, CLOUD_Y + CLOUD_PADDING);
-  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_PADDING, CLOUD_Y + CLOUD_PADDING * 2);
-
-  var maxTime = getMaxElement(times);
-
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(GISTO_X, GISTO_Y, BAR_WIDTH, barHeight);
+var renderGistogram = function(ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
-    var barHeight = (BAR_MAX_HEIGHT * times[i]) / maxTime;
 
     ctx.fillStyle = getBarColor(players[i]);
-    ctx.fillRect(GISTO_X + (BAR_WIDTH + BAR_PADDING) * i, GISTO_Y + (BAR_MAX_HEIGHT - barHeight), BAR_WIDTH, barHeight);
+    ctx.fillRect(GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + (BAR.maxHeight - barHeight), BAR.width, barHeight);
 
 
     var roundedTime = Math.round(times[i]);
     ctx.fillStyle = '#000';
-    ctx.fillText(roundedTime, GISTO_X + (BAR_WIDTH + BAR_PADDING) * i, GISTO_Y + (BAR_MAX_HEIGHT - barHeight) - CLOUD_PADDING);
-    ctx.fillText(players[i], GISTO_X + (BAR_WIDTH + BAR_PADDING) * i, GISTO_Y + BAR_MAX_HEIGHT + CLOUD_PADDING);
+    ctx.fillText(roundedTime, GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + (BAR.maxHeight - barHeight) - CLOUD.padding);
+    ctx.fillText(players[i], GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + BAR.maxHeight + CLOUD.padding);
 
   }
+};
+*/
 
+
+window.renderStatistics = function (ctx, players, times) {
+  // Рисуем облако
+  renderCloud(ctx, CLOUD.x, CLOUD.y, '#fff');
+
+  // Рисуем вступительный текст
+  renderIntro(ctx, 'Ура вы победили!', CLOUD.x + CLOUD.padding, CLOUD.y + CLOUD.padding, '16px PT Mono', '#000');
+  renderIntro(ctx, 'Список результатов:', CLOUD.x + CLOUD.padding, CLOUD.y + CLOUD.padding * 2, '16px PT Mono', '#000');
+  // Рисуем гистограмму
+  /*
+  renderGistogram(ctx, players, times);
+  renderResults(ctx, players, times);
+
+  var maxTime = getMaxElement(times);
+  var barHeight = (BAR.maxHeight * times[i]) / maxTime; */
+
+  var maxTime = getMaxElement(times);
+
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillRect(GISTO_X, GISTO_Y, BAR.width, barHeight);
+
+  for (var i = 0; i < players.length; i++) {
+    var barHeight = (BAR.maxHeight * times[i]) / maxTime;
+
+    ctx.fillStyle = getBarColor(players[i]);
+    ctx.fillRect(GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + (BAR.maxHeight - barHeight), BAR.width, barHeight);
+
+
+    var roundedTime = Math.round(times[i]);
+    ctx.fillStyle = '#000';
+    ctx.fillText(roundedTime, GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + (BAR.maxHeight - barHeight) - CLOUD.padding);
+    ctx.fillText(players[i], GISTO_X + (BAR.width + BAR.padding) * i, GISTO_Y + BAR.maxHeight + CLOUD.padding);
+
+  }
 
 };
